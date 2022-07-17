7b53ac625929ea343aa9e5543dc78bfa5cff8f33
Emerson Rockman <emerson_tecno@hotmail.com>
2022-07-17 10:58:15 -0300

    Procedures JPA

---
+++ src/test/java/com/proceduresjpa/StoredProceduresJPATests.java
@@ -0,0 +1,13 @@
+package com.proceduresjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoredProceduresJPATests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
